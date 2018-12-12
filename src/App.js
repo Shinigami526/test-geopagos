4547d72806a377f0adca960c0b36ab0c21404842
Shinigami526 <twitch7894@gmail.com>
2018-12-12 12:00:47 -0300

    add eslint and more

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React,{ Component } from 'react';
-
+import React, { Component } from "react";
 
 class App extends Component {
-    render() {
-        return (
-            <h1>HELLO WORLD</h1>
-        )
-    }
+  render() {
+    return <h1>HELLO WORLD</h1>;
+  }
 }
 
-export default App
+export default App;
