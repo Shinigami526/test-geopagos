1325193ffdedb50c23fbc2300a5502996d9215a2
Shinigami526 <twitch7894@gmail.com>
2018-12-13 11:05:36 -0300

    add component

---
+++ src/Header/Header.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  _isActive(step, index) {
+    return step === index ? 'active' : '';
+  }
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div className="steps-indicator">
+        <div className={`steps-indicator-item ${this._isActive(step, 0)}`}>
+          1
+        </div>
+        <div className="steps-indicator-separator" />
+        <div className={`steps-indicator-item ${this._isActive(step, 1)}`}>
+          2
+        </div>
+        <div className="steps-indicator-separator" />
+        <div className={`steps-indicator-item ${this._isActive(step, 2)}`}>
+          3
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
